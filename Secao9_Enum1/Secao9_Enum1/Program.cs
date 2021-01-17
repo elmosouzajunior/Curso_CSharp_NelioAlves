4cbb6252377e74212efc60184aa15054f8d528bc
Elmo Junior <elmosouzajunior@gmail.com>
2021-01-17 12:46:23 -0300

    Exemplo Enum_1

---
+++ Secao9_Enum1/Secao9_Enum1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Secao9_Enum1.Entities;
+using Secao9_Enum1.Entities.Enums;
+
+namespace Secao9_Enum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order { Id = 1080, Moment = DateTime.Now, Status = OrderStatus.PendingPayment };
+
+            Console.WriteLine(order);
+        }
+    }
+}
