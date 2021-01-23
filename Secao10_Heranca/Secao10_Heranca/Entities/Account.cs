6206fa0fd2270379e15486d2d643daf4b9e93b91
Elmo Junior <elmosouzajunior@gmail.com>
2021-01-23 12:12:20 -0300

    Exemplo Account implementado

---
+++ Secao10_Heranca/Secao10_Heranca/Entities/Account.cs
@@ -0,0 +1,38 @@
+﻿namespace Secao10_Heranca.Entities
+{
+    class Account
+    {
+        public int Number { get; protected set; }
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder)
+        {
+            Number = number;
+            Holder = holder;
+        }
+
+        public Account(int number, string holder, double balance) : this(number, holder)
+        {
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+
+    }
+
+
+}
---
+++ Secao10_Heranca/Secao10_Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,23 @@
+﻿using Secao10_Heranca.Entities;
+
+namespace Secao10_Heranca.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; protected set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
