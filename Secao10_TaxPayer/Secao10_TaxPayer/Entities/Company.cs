74860cfaf6d1ae9ee93428672426a84898823bf1
Elmo Junior <elmosouzajunior@gmail.com>
2021-01-23 18:56:20 -0300

    Exercicio Tax Payer concluído

---
+++ Secao10_TaxPayer/Secao10_TaxPayer/Entities/Company.cs
@@ -0,0 +1,30 @@
+﻿using Secao10_TaxPayer.Entities;
+
+namespace Secao10_TaxPayer.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company()
+        {
+        }
+
+        public Company(string name, double anualIncome, int numberOfEmployees) : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double TaxCalculator()
+        {
+            if (NumberOfEmployees > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
---
+++ Secao10_TaxPayer/Secao10_TaxPayer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Secao10_TaxPayer.Entities;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Secao10_TaxPayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or Company (C/I): ");
+                char typeOfPayer = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (typeOfPayer == 'I')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployess = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualIncome, numberOfEmployess));
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("TAXES PAID:");
+            foreach(TaxPayer payer in list)
+            {
+                Console.WriteLine(payer.Name + ": $ " + payer.TaxCalculator().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            Console.WriteLine();
+            double totalTax = 0.0;
+            foreach (TaxPayer payer in list)
+            {
+                totalTax += payer.TaxCalculator();
+            }
+            Console.Write("TOTAL TAXES: " + totalTax.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
