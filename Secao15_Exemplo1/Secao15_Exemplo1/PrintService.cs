993b879abf42c28f3516cafbd77ded4d6f762f79
Elmo Junior <elmosouzajunior@gmail.com>
2021-01-30 20:07:16 -0300

    Seção 15 - Exemplo 1

---
+++ Secao15_Exemplo1/Secao15_Exemplo1/PrintService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Secao15_Exemplo1
+{
+    class PrintService
+    {
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public void AddValue(int value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("Print service is full");
+            }
+            else
+            {
+                _values[_count] = value;
+                _count++;
+            }
+        }
+        
+        public int First()
+        {
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count; i++)
+            {
+                if (i < _count - 1)
+                {
+                    Console.Write(_values[i] + ", ");
+                }
+                else
+                {
+                    Console.Write(_values[i]);
+                }
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Secao15_Exemplo1/Secao15_Exemplo1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Secao15_Exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many values? ");
+            int values = int.Parse(Console.ReadLine());
+            PrintService printService = new PrintService();
+            for(int i = 0; i < values; i++)
+            {
+                printService.AddValue(int.Parse(Console.ReadLine()));
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());            
+        }
+    }
+}
